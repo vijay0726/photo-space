20d64810cf75988ecb1e67c6f779b93360189123
vijay0726 <2623312985@qq.com>
2024-08-09 23:47:43 +0800

    chore: fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  publicDir: path.resolve(__dirname, "public"),
   plugins: [
     vue(),
     vueJsx(),
